890ae8b62152d2094f0a5a27607ee1010d108679
Titouan <75045140+PngHash-Titouan@users.noreply.github.com>
2022-07-21 15:43:00 +0200

    Tests newton_method.py

--- tests.py
+++ tests.py
@@ -7,16 +7,33 @@ import newton_method
 
 
 class TestNewton(unittest.TestCase):
-    def test_classic_1(self):
+
+    def test_classic(self):
         pol = sympy.Poly(x**5 + x**2 - x - 0.2)
         initial = 1.3
         iterations = 1
         result = newton_method.apply_newtons_method(pol, pol.diff(),  initial, iterations)
         self.assertAlmostEqual(result, 1.054, 3)
 
-    def test_complex_1(self):
+    def test_complex(self):
+        pol = sympy.Poly(x ** 5 + x ** 2 - x + 1)
+        initial = (-0.5, 0.5)
+        iterations = 1
+        result = newton_method.apply_newtons_method_complex(pol, pol.diff(), initial, iterations)
+        self.assertAlmostEqual(result[0], 0.054, 3)
+        self.assertAlmostEqual(result[1], 0.324, 3)
+
+    def test_classic_multi_iterations(self):
         pol = sympy.Poly(x ** 5 + x ** 2 - x - 0.2)
         initial = 1.3
-        iterations = 1
+        iterations = 4
         result = newton_method.apply_newtons_method(pol, pol.diff(), initial, iterations)
-        self.assertAlmostEqual(result, 1.054, 3)
+        self.assertAlmostEqual(result, 0.812, 3)
+
+    def test_complex_multi_iterations(self):
+        pol = sympy.Poly(x ** 5 + x ** 2 - x + 1)
+        initial = (-0.5, 0.5)
+        iterations = 6
+        result = newton_method.apply_newtons_method_complex(pol, pol.diff(), initial, iterations)
+        self.assertAlmostEqual(result[0], 0.662, 3)
+        self.assertAlmostEqual(result[1], 0.562, 3)
